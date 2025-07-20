9f59f864e8e215204bcac51d9717d7924937c2dd
Suril Waghmare <waghmaresuril@gmail.com>
2025-07-20 15:31:49 -0400

    Fixed build errors

--- src/app/fileHandling/ReadEnv.cpp
+++ src/app/fileHandling/ReadEnv.cpp
@@ -12,7 +12,7 @@ class ReadEnv{
             IO io(env_path);
             std::fstream f_stream = io.getFileStream(); 
             std::stringstream buffer; 
-            buffer << f_stream.rbuf(); 
+            buffer << f_stream.rdbuf(); 
             std::string content = buffer.str();  
             return content; 
         }
